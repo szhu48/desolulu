b17a5b68b4eab4f07723bd1c224a5b60a0ad42e8
meerashroff <mshroff@vassar.edu>
2024-04-30 18:16:45 +0200

    starting title page

--- desolulu/App.js
+++ desolulu/App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.title}>
+        <Text>welcome to (de)solulu</Text>
+      </View>
+      <View style={styles.buttons}>
+        <Pressable>
+          <Text> Log In </Text>
+        </Pressable>
+        <Pressable>
+          <Text> Create Account </Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: "60px",
+    backgroundColor: "#FF46B5",
+  },
+  buttons: {
+    alignItems: "flex-start",
+    backgroundColor: "DEA2DF",
   },
 });
